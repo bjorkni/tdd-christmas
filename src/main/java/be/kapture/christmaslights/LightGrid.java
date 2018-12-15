068ac4d2c2995b962643d307ed53e2b143720234
niklas <niklas.bjorklund@kapture.be>
2018-12-15 17:26:08 +0100

    add actual grid

--- src/main/java/be/kapture/christmaslights/LightGrid.java
+++ src/main/java/be/kapture/christmaslights/LightGrid.java
@@ -1,14 +1,27 @@
 package be.kapture.christmaslights;
 
+import java.util.stream.IntStream;
+
 public class LightGrid {
 
-    private int numberOfLightsOn;
+    private static final int GRID_SIZE = 1000;
 
-    public int getNumberOfLightsOn() {
-        return numberOfLightsOn;
+    private boolean[][] grid = new boolean[GRID_SIZE][GRID_SIZE];
+
+    public long getNumberOfLightsOn() {
+        return IntStream.range(0, GRID_SIZE)
+                .mapToLong(x ->
+                        IntStream.range(0, GRID_SIZE)
+                                .mapToObj(y -> grid[x][y])
+                                .filter(Boolean::booleanValue)
+                                .count())
+                .sum();
+
     }
 
-    public void turnOn(int x1, int x2, int y1, int y2) {
-        numberOfLightsOn = 1;
+    public void turnOn(int x1, int y1, int x2, int y2) {
+        IntStream.rangeClosed(x1, x2)
+                .forEach(x -> IntStream.rangeClosed(y1, y2)
+                        .forEach(y -> grid[x][y] = true));
     }
 }
--- src/test/java/be/kapture/christmaslights/LightGridTest.java
+++ src/test/java/be/kapture/christmaslights/LightGridTest.java
@@ -1,24 +1,49 @@
 package be.kapture.christmaslights;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class LightGridTest {
 
+    private LightGrid lightGrid;
+
+    @Before
+    public void setUp() {
+        lightGrid = new LightGrid();
+    }
+
     @Test
     public void initialState_allOff() {
-        LightGrid lightGrid = new LightGrid();
-
-        assertThat(lightGrid.getNumberOfLightsOn()).isEqualTo(0);
+        expectOn(0);
     }
 
     @Test
     public void turnOnOne() {
-        LightGrid lightGrid = new LightGrid();
+        lightGrid.turnOn(0, 0, 0, 0);
 
-        lightGrid.turnOn(0, 0, 1, 1);
-
-        assertThat(lightGrid.getNumberOfLightsOn()).isEqualTo(1);
+        expectOn(1);
     }
+
+    @Test
+    public void turnOnAll() {
+        lightGrid.turnOn(0, 0, 999, 999);
+
+        expectOn(1_000_000);
+    }
+
+    @Test
+    public void turnOnTwoLines() {
+        lightGrid.turnOn(0, 0, 999, 0);
+        lightGrid.turnOn(0, 8, 999, 8);
+
+        expectOn(2000);
+    }
+
+    private void expectOn(int ex) {
+        assertThat(lightGrid.getNumberOfLightsOn()).isEqualTo(ex);
+    }
+
+
 }
